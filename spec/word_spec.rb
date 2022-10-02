b8ac8683dc39d3a714da8da2037dfcdfaffd194a
Anthony Gladden <a.gladden360@outlook.com>
2022-10-02 08:38:40 -0700

    fixed word_spec

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -10,16 +10,16 @@ describe '#word' do
 
 describe('#inittialize') do
     it('it will recongnize name and id') do 
-      words = Word.new("Grant") 
+      words = Word.new("Grant", nil) 
     expect(words.name).to(eq("Grant"))
   end 
 end
 
 describe('#save') do
   it('saves a word') do
-    words = Word.new("Grant" )
+    words = Word.new("Grant", nil)
       words.save()
-      words2 = Word.new("Bill" ) 
+      words2 = Word.new("Bill", nil) 
       words2.save()
       expect(Word.all).to(eq([words, words2]))
   end
@@ -33,17 +33,17 @@ end
 
 describe('#==') do 
   it('if word matches another word') do
-    words = Word.new('dog' )
-    words2 = Word.new('dog' )
+    words = Word.new('dog', nil)
+    words2 = Word.new('dog', nil)
     expect(words).to(eq(words2))
   end
 end
 
 describe('.clear') do 
   it('clear all words') do 
-    words = Word.new("Grant" )
+    words = Word.new("Grant", nil )
     words.save()
-    words2 = Word.new("Bill" )
+    words2 = Word.new("Bill", nil )
     words2.save()
     Word.clear()
     expect(Word.all).to(eq([]))
