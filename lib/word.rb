a65fb82fbf6374401d38085e7163a4f19d5970f1
Anthony Gladden <a.gladden360@outlook.com>
2022-10-01 18:52:00 -0700

    added save test and function

--- lib/word.rb
+++ lib/word.rb
@@ -1,1 +1,19 @@
 require('pry')
+
+class Word
+  attr_reader :id, :name
+
+  @@words = {}
+  @@total_row = 0
+
+  def inittialize(name, id)
+    @name = name
+    @id = id || @@total_row += 1
+  end
+
+  def save()
+    @@words[self.id] = Word.new(self.name, self.id)
+  end
+
+
+end
---
+++ spec/word_spec.rb
@@ -0,0 +1,37 @@
+require('rspec')
+require('pry')
+require('word')
+
+describe '#word' do 
+
+  # before(:each) do 
+  #   Word.clear()
+  # end
+
+describe('#inittialize') do
+    it('it will recongnize name and id') do 
+      words = Word.new
+      ("Grant")
+    expect(words.name).to(eq("Grant"))
+  end 
+end
+
+describe('#save') do
+  it('saves a word') do
+    words = Word.new
+    ("Grant")
+      words.save()
+      words2 = Word.new("Bill") 
+      words2.save()
+      expect(Word.all).to(eq([words, words2]))
+  end
+end
+
+
+
+
+
+
+
+
+end
